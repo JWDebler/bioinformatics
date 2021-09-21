d9a6fbf7722f1a97fede84b83bfe622453302946
Johannes Debler <johannes.debler@gmail.com>
2021-09-21 13:44:22 +0800

    tests

--- pante_to_genbank.py
+++ pante_to_genbank.py
@@ -130,6 +130,9 @@ with open(input_file) as file:
                         search = re.search('repeat_unit=(.+?)$', elements[8])
                         if search:
                             rpt_unit = search.group(1)
+                            if elements[4]-elements[3] < rpt_unit.length():
+                                print("ERROR")
+                                break
                             elements[8] += "; rpt_type=" + rpt_type + "; satellite_type=microsatellite" + "; rpt_unit_seq=" + rpt_unit
                     else:
                         elements[8] += "; rpt_type=" + rpt_type + "; satellite_type=microsatellite"
