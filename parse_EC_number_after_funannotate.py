77b5c7cd15a7625a8056b486f9a727ff598ab987
Johannes Debler <johannes.debler@gmail.com>
2022-02-07 16:21:45 +0800

    parsing using file instead of individual requests

--- parse_EC_number_after_funannotate.py
+++ parse_EC_number_after_funannotate.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 import os
+import urllib.request
 import csv
 import argparse
 import re
@@ -20,16 +21,50 @@ else:
     print("No input file provided, use '-i' and supply a .gff file")
     #raise SystemExit
 
-def scrape_ec(ec):
-    url = "https://enzyme.expasy.org/EC/" + ec
-    html_content = requests.get(url).text
-    requests.session().close()
-    soup = BeautifulSoup(html_content, "lxml")
-    ecnumber = soup.title.prettify()
-    #parse only the name out of the title
-    enzyme_name = (ecnumber[(ecnumber.find(ec))+len(ec)+1:-10]).replace(",","%2C")# same change for existing names in GFF
-    return enzyme_name
+# def scrape_ec(ec):
+#     url = "https://enzyme.expasy.org/EC/" + ec
+#     html_content = requests.get(url).text
+#     requests.session().close()
+#     soup = BeautifulSoup(html_content, "lxml")
+#     table = soup.find_all('table')[0]
+#     rows = []
+#     for child in table.children:
+#         row = []
+#         for td in child:
+#             try:
+#                 row.append(td.text.replace('\n', ''))
+#             except:
+#                 continue
+#         if len(row) > 0:
+#             rows.append(row)
 
+#     enzyme_name_raw = rows[1][0]
+#     enzyme_name = enzyme_name_raw[0:-1].replace(",","%2C")
+    
+#     # old attempt
+#     #ecnumber = soup.title.prettify()
+#     #enzyme_name = (ecnumber[(ecnumber.find(ec))+len(ec)+1:-10]).replace(",","%2C")# same change for existing names in GFF
+#     print(ec, enzyme_name)
+#     return enzyme_name
+
+#download https://ftp.expasy.org/databases/enzyme/enzyme.dat
+#and parse into a dictionary
+enzyme_url = "https://ftp.expasy.org/databases/enzyme/enzyme.dat"
+urllib.request.urlretrieve(enzyme_url, "enzyme.dat")
+
+current_id = ''
+current_name = ''
+enzyme_dict = {}
+enzyme_file = Path("enzyme.dat") 
+
+with open(enzyme_file) as file:
+    lines = file.readlines()
+    for line in lines:
+        if line.startswith('ID'):
+            current_id = line[4:].strip()
+        if line.startswith('DE'):
+            current_name = line[4:-2].strip().replace(",","%2C")
+        enzyme_dict[current_id] = current_name
 
 
 with open(input_file) as file:
@@ -44,11 +79,12 @@ with open(input_file) as file:
             #parse EC number
             search = re.search('[eE]C_number=(.+?)$', elements[8])
             ec=search.group(1).split(';')[0]
-            #print(ec) #debug
+           
             #get enzyme name
             # if EC number is incomplete (1, 1.1 or 1.1.1 instead of 1.1.1.1), don't bother looking it up
             if ec.count('.') == 3:
-                enzyme_name = scrape_ec(ec)
+                enzyme_name = enzyme_dict[ec]
+                #enzyme_name = scrape_ec(ec) #old method
                 #print('scrape =====> all good') #debug
             else:
                 enzyme_name = ''                
@@ -98,4 +134,4 @@ with open(input_file) as file:
                 print(*elements, sep='\t')
         else:
             print(*elements, sep='\t')
-            #continue
+            continue
