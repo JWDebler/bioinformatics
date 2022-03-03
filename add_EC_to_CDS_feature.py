a3e6f7e5fc2f0f49cb9035bb35eb0509c901ad0f
Johannes Debler <johannes.debler@gmail.com>
2022-03-03 14:56:50 +0800

    fixes for missing product tag and hashes in gff file

--- add_EC_to_CDS_feature.py
+++ add_EC_to_CDS_feature.py
@@ -13,7 +13,7 @@ args = parser.parse_args()
 if args.input:
     input_file = Path(args.input)
 else:
-    input_file = Path("test.gff")
+    input_file = Path("test1.gff3")
     print("No input file provided, use '-i' and supply a .gff file")
     #raise SystemExit
 
@@ -21,19 +21,21 @@ with open(input_file) as file:
     input = csv.reader(file, delimiter='\t')
     id_product_mapper = {}
     for line in input:
+        if line[0].startswith('#'):
+            continue
         elements = []
         
         for element in line:
             elements.append(element)
+        
         # fill dictionary with EC number and matching ID
         if "product=" in elements[8]:
-            #print(elements[8]) debug
+            #print(elements[8]) #debug
             product_string = re.search('product=(.+?)$', elements[8])   
             id_string = re.search('ID=(.+?)$', elements[8]) 
             product=product_string.group(1).split(';')[0]
             id=id_string.group(1).split(';')[0]
-            #print(product)
-            #print(id)
+            #print(product, id) debug
             id_product_mapper[id] = 'product='+product+';'
 
 #print(id_product_mapper)
@@ -41,18 +43,22 @@ with open(input_file) as file:
 with open(input_file) as file:   
     input = csv.reader(file, delimiter='\t')
     for line in input:
+        if line[0].startswith('#'):
+            continue
         elements = []
         for element in line:
             elements.append(element)
         if elements[2] == 'CDS':
-            #print('before:', line)
+            #print('before:', line)     #debug
             id_string = re.search('ID=(.+?)$', elements[8]) 
             id=id_string.group(1).split(';')[0]
             id = id.split('.')[0]
-            elements[8] += id_product_mapper[id]   
-            #print('after: ', elements)   
+            if id in id_product_mapper:
+                elements[8] += ';' + id_product_mapper[id]   
+            #print('after: ', elements)    #debug
             print(*elements, sep='\t')
         else:
+            #print('---') #debug
             print(*elements, sep='\t')
 
 
