5f16d7f6ba1f7225f4658697c12ab23340f0b441
Johannes Debler <johannes.debler@gmail.com>
2022-05-19 09:39:40 +0800

    LINE RTE RTE added

--- pante_to_genbank.py
+++ pante_to_genbank.py
@@ -191,6 +191,10 @@ with open(input_file) as file:
                     elif name == "LINE/RTE/X":
                         elements[2] = "mobile_element"
                         elements[8] += "; mobile_element_type=LINE:X "
+                                                                        
+                    elif name == "LINE/RTE/RTE":
+                        elements[2] = "mobile_element"
+                        elements[8] += "; mobile_element_type=LINE:RTE "
 
                     elif name == "LINE/CR1":
                         elements[2] = "mobile_element"
