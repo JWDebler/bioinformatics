38665cc338266e039e36c69cfd3129956d17b7b0
Johannes Debler <johannes.debler@gmail.com>
2022-08-19 14:31:48 +0800

    trna

--- pante_to_genbank.py
+++ pante_to_genbank.py
@@ -416,7 +416,7 @@ with open(input_file) as file:
                                 elements[2] = "gene"
                                 gene = trnas[x][1]
                                 id = p.split("=",1)[1]
-                                id1 = id.split("_",1)[1]
+                                id1 = id.split("_",1)[0]
                                 elements[8] = "ID=gene." + id1 + "; gene=" + gene + "; pseudo=true; pseudogene=unknown"
                                 print(*elements, sep='\t')
                                 elements[2] = "trna"
